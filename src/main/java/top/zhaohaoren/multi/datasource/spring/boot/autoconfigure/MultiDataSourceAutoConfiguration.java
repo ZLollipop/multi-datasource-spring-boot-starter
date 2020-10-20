1f1e3b4982d0fb355628ab879c9715a8b2756db5
zhaohaoren <zhaohaoren666@gmail.com>
2020-10-21 01:00:00 +0800

    first commit

---
+++ src/main/java/top/zhaohaoren/multi/datasource/spring/boot/autoconfigure/MultiDataSourceAutoConfiguration.java
@@ -0,0 +1,11 @@
+package top.zhaohaoren.multi.datasource.spring.boot.autoconfigure;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author zhaohaoren
+ */
+@Configuration
+public class MultiDataSourceAutoConfiguration {
+
+}
